4431e2af3424b08b26dd6cb3904332d424b274ea
NguyenDat-2003 <maxdat2003@gmail.com>
2024-02-22 15:53:14 +0700

    Completed refactor_project_structure

---
+++ src/pages/Boards/BoardContent/index.jsx
@@ -0,0 +1,21 @@
+import Box from '@mui/material/Box'
+
+function BoardContent() {
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: 'primary.main',
+          width: ' 100%',
+          height: (theme) => `calc(100vh - ${theme.trello.headerBarheight} - ${theme.trello.boardBarheight})`,
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        Board Content
+      </Box>
+    </>
+  )
+}
+
+export default BoardContent
